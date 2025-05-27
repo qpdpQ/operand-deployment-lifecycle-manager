a616092d7cd0a96dff091ae009b30856a452f274
Allen Li <liyuchen223@gmail.com>
2025-05-27 11:50:59 -0400

    controller runtime version bump
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- main.go
+++ main.go
@@ -103,9 +103,7 @@ func main() {
 
 	options := ctrl.Options{
 		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
 		HealthProbeBindAddress: probeAddr,
-		Port:                   9443,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "ab89bbb1.ibm.com",
 	}
