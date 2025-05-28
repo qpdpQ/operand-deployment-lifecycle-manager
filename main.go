c965cbaa11f9d408f77046d13eb85f33c257c4b6
Allen Li <liyuchen223@gmail.com>
2025-05-28 11:46:39 -0700

    update watchNamespaces
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- main.go
+++ main.go
@@ -128,6 +128,11 @@ func main() {
 		}
 	} else {
 		// cache resource in watchNamespaces
+		defaultNamespaces := make(map[string]cache.Config)
+		for _, ns := range watchNamespaces {
+			defaultNamespaces[ns] = cache.Config{}
+		}
+
 		managedCache = cache.Options{
 			ByObject: map[client.Object]cache.ByObject{
 				&corev1.Secret{}: {
@@ -146,7 +151,7 @@ func main() {
 					Label: cacheFreshLabelSelector,
 				},
 			},
-			DefaultNamespaces: map[string]cache.Config{watchNamespace: {}},
+			DefaultNamespaces: defaultNamespaces,
 		}
 	}
 
